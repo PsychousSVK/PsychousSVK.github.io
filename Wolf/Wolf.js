250def6565d1a682ddb8c250c6f75c1b232c5f55
PsychoušSvK <62697416+PsychousSVK@users.noreply.github.com>
2020-04-03 16:03:00 +0200

    Update Wolf.js

--- Wolf/Wolf.js
+++ Wolf/Wolf.js
@@ -2340,6 +2340,42 @@ zvireCommand: {
                     }
                 }
             },
+koupitCommand: {
+                command: ['koupit', 'buy'],
+                rank: 'user',
+                type: 'startsWith',
+                getobchod: function (chat) {
+                    var c = Math.floor(Math.random() * basicBot.chat.obchod.length);
+                    return basicBot.chat.obchod[c];
+                },
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        var msg = chat.message;
+
+                        var space = msg.indexOf(' ');
+                        if (space === -1) {
+                            API.sendChat(basicBot.chat.givekoupit);
+                            return false;
+                        }
+                        else {
+                            var name = msg.substring(space + 2);
+                            var user = basicBot.userUtilities.lookupUserName(name);
+                            if (user === false || !user.inRoom) {
+                                return API.sendChat(subChat(basicBot.chat.nouserkoupit, {name: name}));
+                            }
+                            else if (user.username === chat.un) {
+                                return API.sendChat(subChat(basicBot.chat.selfkoupit, {name: name}));
+                            }
+                            else {
+                                return API.sendChat(subChat(basicBot.chat.koupit, {nameto: user.username, namefrom: chat.un, OBCHOD: this.getobchod()}));
+                            }
+                        }
+                    }
+                }
+            },
+
 
             luluCommand: {
                 command: 'lulu',
