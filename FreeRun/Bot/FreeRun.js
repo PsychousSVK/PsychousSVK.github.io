68f0ad690cea29e3130bc43ba375c2b62deffb70
PsychoušSvK <62697416+PsychousSVK@users.noreply.github.com>
2020-11-17 18:52:27 +0100

    Update FreeRun.js

--- FreeRun/Bot/FreeRun.js
+++ FreeRun/Bot/FreeRun.js
@@ -245,7 +245,7 @@
     var botCreatorIDs = [3851534, 4105209];
 
     var basicBot = {
-        version: '3.13.5',
+        version: '3.13.6',
         status: false,
         name: 'Foxíček',
         loggedInID: null,
